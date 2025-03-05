b2a6cfb77816fc8ab26be3657eb165b83820a40b
Janderson-Mota <jandersonmota2407@gmail.com>
2025-03-04 21:56:43 -0300

    Atualização 15

---
+++ Unidades Curriculares/Programação de soluções computacionais/Exercicios/Aulas_Teoricas/ExtruturaSequencial/ListaDeExercicios/Exercicio_1.py
@@ -0,0 +1,6 @@
+n1 = float(input('Digite o primeiro para obter a média'))
+n2 = float(input('Digite o segundo para obter a média'))
+n3 = float(input('Digite o terceiro para obter a média'))
+
+media = (n1 + n2 + n3) / 3
+print("A media dos valores citados são: ", media)
